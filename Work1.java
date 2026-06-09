315e68df98b3edfd151eced0b0dbb5e1ef322faa
dkonadu037-cell <dkonadu037@gmail.com>
2026-06-09 12:26:48 +0100

    Add files via upload

---
+++ Work1.java
@@ -0,0 +1,11 @@
+public class Work1 {
+    public static void main(String[] args){
+        System.out.println("x                                    x");
+        System.out.println("x                                    x");
+        System.out.println("x            xxxxxxxxxx              x");
+        System.out.println("xxxxxxx      x        x       xxxxxxxx");
+        System.out.println("x     x      x        x       x      x");
+        System.out.println("x     x      x        x       x      x");
+        System.out.println("x     x      x        x       x      x");
+    }
+}
