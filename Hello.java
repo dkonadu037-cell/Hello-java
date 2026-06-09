5de3016cd5632371b0f5d8f310fa638c52860c33
dkonadu037-cell <dkonadu037@gmail.com>
2026-06-09 12:04:46 +0100

    Add files via upload

--- Hello.java
+++ Hello.java
@@ -1,5 +1,11 @@
 public class Hello {
-	public static void main(String[] args){
-	System.out.print("Hello W`orld");
-}
+    public static void main(String[] args){
+        System.out.println("x                                    x");
+        System.out.println("x                                    x");
+        System.out.println("x            xxxxxxxxxx              x");
+        System.out.println("xxxxxxx      x        x       xxxxxxxx");
+        System.out.println("x     x      x        x       x      x");
+        System.out.println("x     x      x        x       x      x");
+        System.out.println("x     x      x        x       x      x");
+    }
 }
